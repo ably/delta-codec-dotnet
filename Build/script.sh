6501211496ef5d3d0f1a037d5a55fbc9a24e1898
Tsviatko Yovtchev <tsviatko@codemerx.com>
2019-07-29 19:35:06 +0300

    Fixing build

--- Build/script.sh
+++ Build/script.sh
@@ -2,8 +2,8 @@
 
 if [ "$DOTNETCORE" == 'true' ]
 then
-	dotnet test
+	dotnet test --framework netcoreapp2.0
 else
 	msbuild /p:Configuration=Release MiscUtil.Compression.Vcdiff.sln
-	mono ./testrunner/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe ./MiscUtil.Compression.Vcdiff.Test/bin/Release/MiscUtil.Compression.Vcdiff.Test.dll
+	mono ./testrunner/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe ./MiscUtil.Compression.Vcdiff.Test/bin/Release/net46/MiscUtil.Compression.Vcdiff.Test.dll
 fi
