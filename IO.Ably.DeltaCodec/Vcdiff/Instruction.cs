681a5edcfa7b2016b01511affda00d03262185b0
Tom Kirby-Green <tomkirbygreen@gmail.com>
2021-09-24 12:45:23 +0100

    'Instruction' make 'readonly' and tidy

--- IO.Ably.DeltaCodec/Vcdiff/Instruction.cs
+++ IO.Ably.DeltaCodec/Vcdiff/Instruction.cs
@@ -1,37 +1,21 @@
-
 namespace IO.Ably.DeltaCodec.Vcdiff
 {
 	/// <summary>
 	/// Contains the information for a single instruction
 	/// </summary>
-	internal struct Instruction
+	internal readonly struct Instruction
 	{
-		readonly InstructionType type;
-		internal InstructionType Type
-		{
-			get { return type; }
-		}
+		internal InstructionType Type { get; }
 
-		readonly byte size;
-		internal byte Size
-		{
-			get { return size; }
-		}
+		internal byte Size { get; }
 
-		readonly byte mode;
-		internal byte Mode
-		{
-			get { return mode; }
-		}
+		internal byte Mode { get; }
 
 		internal Instruction(InstructionType type, byte size, byte mode)
 		{
-			this.type = type;
-			this.size = size;
-			this.mode = mode;
+			Type = type;
+			Size = size;
+			Mode = mode;
 		}
-
-
 	}
 }
-
